3a6edbd3cca726b7ba743346651240a522d7a1ac
Junyou333 <jyo336633@gmail.com>
2021-08-23 12:22:47 +0800

    會員中心_navbar 登入帳號判斷

--- mid_projrct/index_.php
+++ mid_projrct/index_.php
@@ -1,9 +1,11 @@
 <?php
 include __DIR__ . '/partials/init.php';
 $title = '美食區';
-$sql="SELECT * FROM `account_list` WHERE `sid`='$sid';";
-$datai = $pdo->query($sql)->fetch();
-echo $datai;
+if (isset($_SESSION['sid'])) {
+    $sql = "SELECT * FROM `account_list` WHERE `sid`='$sid';";
+    $datai = $pdo->query($sql)->fetch();
+    exit;
+}
 ?>
 <?php include __DIR__ . '/partials/html-head.php'; ?>
 <?php include __DIR__ . '/partials/mid_navbar.php'; ?>
