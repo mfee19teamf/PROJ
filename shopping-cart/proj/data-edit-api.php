3f266316289c1187be73f7c286822fbf52f37c64
CHEN YORONG <d994421040@gmail.com>
2021-08-24 12:46:36 +0800

    edit 檢查刪除

--- shopping-cart/proj/data-edit-api.php
+++ shopping-cart/proj/data-edit-api.php
@@ -12,25 +12,12 @@ $output=[
 
 ];
 
-if(
-    empty($_POST['sid'])or
-    empty($_POST['name'])or
-    empty($_POST['email'])or
-    empty($_POST['mobile'])or
-    empty($_POST['bithday'])
-    ){
-        $output['error']='有欄位沒有填寫';
-        $output['code']=440;
-        echo json_encode($output,JSON_UNESCAPED_UNICODE);
-        exit;
-    }
-
 
 
 //來源不名一律用這個
 $sql="UPDATE `order` SET 
                         
-                        `memberID`=?,
+                        
                         `name`=?,
                         `mobile`=?,
                         `address`=?,
@@ -39,7 +26,7 @@ $sql="UPDATE `order` SET
 $stmt = $pdo->prepare($sql); //prepare()準備 execute($_POST[''],$_POST[''],)執行 對應每個問號
 $stmt->execute([
     
-    $_POST['memberID'],
+    
     $_POST['name'],
     $_POST['mobile'],
     $_POST['address'],
